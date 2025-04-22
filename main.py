87b3d4b3b0f88fb670b4204b7cdb26b147cd1eb6
lc <lichon@foxmail.com>
2025-04-23 05:22:00 +0800

    fix deps

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
 import pathlib
 import tts
-import keyboard
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 tts_service = tts.LocalTTSService()
-
-
-def on_ctrl_0():
-    tts_service.speak_to_file("紧急情况退出")
-
-def on_ctrl_1():
-    tts_service.speak_to_file("")
-
-def on_ctrl_2():
-    tts_service.speak_to_file("")
-
-def on_ctrl_3():
-    tts_service.speak_to_file("")
-
-def on_ctrl_4():
-    tts_service.speak_to_file("")
-
-def on_ctrl_5():
-    tts_service.speak_to_file("")
-
-def on_ctrl_6():
-    tts_service.speak_to_file("")
-
-def on_ctrl_7():
-    tts_service.speak_to_file("")
-
-def on_ctrl_8():
-    tts_service.speak_to_file("")
-
-def on_ctrl_9():
-    tts_service.speak_to_file("这是 ctontrol 9")
-
-keyboard.add_hotkey("ctrl+alt+0", on_ctrl_0)
-keyboard.add_hotkey("ctrl+alt+1", on_ctrl_1)
-keyboard.add_hotkey("ctrl+alt+2", on_ctrl_2)
-keyboard.add_hotkey("ctrl+alt+3", on_ctrl_3)
-keyboard.add_hotkey("ctrl+alt+4", on_ctrl_4)
-keyboard.add_hotkey("ctrl+alt+5", on_ctrl_5)
-keyboard.add_hotkey("ctrl+alt+6", on_ctrl_6)
-keyboard.add_hotkey("ctrl+alt+7", on_ctrl_7)
-keyboard.add_hotkey("ctrl+alt+8", on_ctrl_8)
-keyboard.add_hotkey("ctrl+alt+9", on_ctrl_9)
 
 @app.get("/", response_class=HTMLResponse)
 def index():
